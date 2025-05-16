5e7420d89307b22607909423d66c9c833eb73877
Millienyts <tmillieny@gmail.com>
2025-05-16 00:13:04 -0400

    files manager

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,27 @@
-import express from 'express';
-import AppController from '../controllers/AppController.js';
-import UsersController from '../controllers/UsersController.js';
-import AuthController from '../controllers/AuthController.js';
-import FilesController from '../controllers/FilesController.js';
+import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 
-const router = express.Router();
+const express = require('express');
 
-router.use(express.json());
+const router = (app) => {
+  const paths = express.Router();
+  app.use(express.json());
+  app.use('/', paths);
 
-router.get('/status', AppController.getStatus);
-router.get('/stats', AppController.getStats);
-router.post('/users', UsersController.postNew);
-router.get('/connect', AuthController.getConnect);
-router.get('/disconnect', AuthController.getDisconnect);
-router.get('/users/me', UsersController.getMe);
-router.post('/files', FilesController.postUpload);
-router.get('/files/:id', FilesController.getShow);
-router.get('/files', FilesController.getIndex);
-router.put('/files/:id/publish', FilesController.putPublish);
-router.put('/files/:id/unpublish', FilesController.putUnpublish);
-router.get('/files/:id/data', FilesController.getFile);
+  paths.get('/status', (request, response) => AppController.getStatus(request, response));
+  paths.get('/stats', (request, response) => AppController.getStats(request, response));
+  paths.post('/users', (request, response) => UsersController.postNew(request, response));
+  paths.get('/connect', (request, response) => AuthController.getConnect(request, response));
+  paths.get('/disconnect', (request, response) => AuthController.getDisconnect(request, response));
+  paths.get('/users/me', (request, response) => UsersController.getMe(request, response));
+  paths.post('/files', (request, response) => FilesController.postUpload(request, response));
+  paths.get('/files/:id', (request, response) => FilesController.getShow(request, response));
+  paths.get('/files', (request, response) => FilesController.getIndex(request, response));
+  paths.put('/files/:id/publish', (request, response) => FilesController.putPublish(request, response));
+  paths.put('/files/:id/unpublish', (request, response) => FilesController.putUnpublish(request, response));
+  paths.get('/files/:id/data', (request, response) => FilesController.getFile(request, response));
+};
 
 export default router;
