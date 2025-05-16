588a55579d3e94dcdb35a223348e22e8d230f733
Millienyts <tmillieny@gmail.com>
2025-05-15 23:58:38 -0400

    files manager

--- utils/redis.mjs
+++ utils/redis.mjs
@@ -1,31 +1,41 @@
-// utils/redis.mjs
-import { createClient } from 'redis';
+import redis from 'redis';
+import { promisify } from 'util';
 
 class RedisClient {
   constructor() {
-    this.client = createClient();
+    this.client = redis.createClient();
+
     this.client.on('error', (err) => {
-      console.error(`Redis client not connected to the server: ${err}`);
+      console.log(`Redis client not connected to the server: ${err}`);
     });
-    this.client.connect().then(() => {
-      console.log('Redis client connected');
+
+    this.client.on('connect', () => {
+      console.log('Redis client connected to the server');
     });
+
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setAsync = promisify(this.client.set).bind(this.client);
+    this.delAsync = promisify(this.client.del).bind(this.client);
+    this.expireAsync = promisify(this.client.expire).bind(this.client);
   }
 
   isAlive() {
-    return this.client.isOpen;
+    return this.client.connected;
   }
 
   async get(key) {
-    return this.client.get(key);
+    return this.getAsync(key);
   }
 
   async set(key, value, duration) {
-    await this.client.setEx(key, duration, value);
+    await this.setAsync(key, value);
+    if (duration) {
+      await this.expireAsync(key, duration);
+    }
   }
 
   async del(key) {
-    await this.client.del(key);
+    await this.delAsync(key);
   }
 }
 
